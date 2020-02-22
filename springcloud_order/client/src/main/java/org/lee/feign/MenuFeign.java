5d54c2da5c23a1c728a41d55924f6d888f5f1fc5
Ctrl08 <390022566@qq.com>
2020-02-22 18:19:18 +0800

    创建menu的服务消费者client并简单完成请求数据测试

---
+++ springcloud_order/client/src/main/java/org/lee/feign/MenuFeign.java
@@ -0,0 +1,15 @@
+package org.lee.feign;
+
+import org.lee.entity.Menu;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(value = "menu")
+public interface MenuFeign {
+
+    @GetMapping("/menu/findAll/{index}/{limit}")
+    public List<Menu> findAll(@PathVariable("index")int index,@PathVariable("limit")int limit);
+}
