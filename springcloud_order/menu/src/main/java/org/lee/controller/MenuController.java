e2a7e2b3f0d1276b596a4daf714bf64ca8715d8e
Ctrl08 <390022566@qq.com>
2020-02-22 10:38:29 +0800

    添加Menu实体类并编写接口及对应sql语句

--- springcloud_order/menu/src/main/java/org/lee/controller/MenuController.java
+++ springcloud_order/menu/src/main/java/org/lee/controller/MenuController.java
@@ -1,9 +1,15 @@
 package org.lee.controller;
 
+import org.lee.entity.Menu;
+import org.lee.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
@@ -12,8 +18,16 @@ public class MenuController {
     @Value("${server.port}")
     private String port;
 
+    @Autowired
+    private MenuRepository menuRepository;
+
     @GetMapping("/index")
     public String index(){
         return "简单测试-------menu的端口："+this.port;
+    }
+
+    @GetMapping("/findAll/{index}/{limit}")
+    public List<Menu> findAll(@PathVariable("index") int index, @PathVariable("limit") int limit){
+        return menuRepository.findAll(index,limit);
     }
 }
---
+++ springcloud_order/menu/src/main/java/org/lee/repository/MenuRepository.java
@@ -0,0 +1,19 @@
+package org.lee.repository;
+
+import org.lee.entity.Menu;
+
+import java.util.List;
+
+public interface MenuRepository {
+    public List<Menu> findAll(int index, int limit);
+
+    public int count();
+
+    public Menu findById(long id);
+
+    public void save(Menu menu);
+
+    public void update(Menu menu);
+
+    public void deleteById(long id);
+}
